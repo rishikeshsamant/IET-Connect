9f31d36820ba29ca184e19cc1ec062b14bba940d
rishikeshsamant <rishikeshsamant@hotmail.com>
2025-04-28 03:42:00 +0530

    Create cors.middleware.js and setup in app.js

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,10 @@
 import express from "express";
+import corsMiddleware from "./middlewares/cors.middleware.js";
 
 const app = express();
 
 app.use(express.json());
+app.use(corsMiddleware);
 
 app.get('/', (req, res) => {
     res.send("Hello world!");
