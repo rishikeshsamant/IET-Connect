cf8c83a78cbab7910b52982f1c10a5f7c43f8175
Yasir Akhlaque <akhlaqueyasir28@gmail.com>
2025-05-04 19:47:34 +0530

    Integrated Login & SignUp Pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,25 @@
 import './App.css'
 import { BrowserRouter as Router, Routes, Route, } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
+import Login from './pages/Login';
+import { createContext, useState } from 'react';
+
+// export const LoginContext = createContext();
+export const SignUpContext = createContext();
 
 function App() {
+  // const [isLoginActive, setIsLoginActive] = useState(false);
+  const [isSignUpActive, setIsSignUpActive] = useState(false);
   return (
     <>
-      <Router>
-        <Routes>
-          <Route path='/' element={<LandingPage />} />
-        </Routes>
-      </Router>
+      <SignUpContext.Provider value={{ isSignUpActive, setIsSignUpActive }}>
+        <Router>
+          <Routes>
+            <Route path='/' element={<LandingPage />} />
+            <Route path='/login' element={<Login />} />
+          </Routes>
+        </Router>
+      </SignUpContext.Provider>
     </>
   )
 }
